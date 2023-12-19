2574e45ef32873011638a7ff64355a33b4e01e5a
chirfan1 <44518884+chirfan1@users.noreply.github.com>
2023-12-19 15:04:30 +0700

    Clock(system): Create Clock using tkinter COMPLETED

--- clock.py
+++ clock.py
@@ -28,6 +28,34 @@ def update_clock():
         else:
             canvas.create_text(x, y, text=str(i), font=("Helvetica", 12))
 
+    for i in range(60):
+        angle = i * math.pi/30 - math.pi/2
+        x1 = WIDTH/2 + 0.8 * WIDTH/2 * math.cos(angle)
+        y1 = HEIGHT/2 + 0.8 * HEIGHT/2 * math.sin(angle)
+        x2 = WIDTH/2 + 0.9 * WIDTH/2 * math.cos(angle)
+        y2 = HEIGHT/2 + 0.9 * HEIGHT/2 * math.sin(angle)
+        if i % 5 == 0:
+            canvas.create_line(x1, y1, x2, y2, fill="black", width=3)
+        else:
+            canvas.create_line(x1, y1, x2, y2, fill="black", width=1)
+
+    hour_angle = (hour + minute/60) * math.pi/6 - math.pi/2
+    hour_x = WIDTH/2 + 0.5 * WIDTH/2 * math.cos(hour_angle)
+    hour_y = HEIGHT/2 + 0.5 * HEIGHT/2 * math.sin(hour_angle)
+    canvas.create_line(WIDTH/2, HEIGHT/2, hour_x, hour_y, fill="black", width=6)
+
+    minute_angle = (minute + second/60) * math.pi/30 - math.pi/2
+    minute_x = WIDTH/2 + 0.7 * WIDTH/2 * math.cos(minute_angle)
+    minute_y = HEIGHT/2 + 0.7 * HEIGHT/2 * math.sin(minute_angle)
+    canvas.create_line(WIDTH/2, HEIGHT/2, minute_x, minute_y, fill="black", width=4)
+
+    second_angle = second * math.pi/30 - math.pi/2
+    second_x = WIDTH/2 + 0.6 * WIDTH/2 * math.cos(second_angle)
+    second_y = HEIGHT/2 + 0.6 * HEIGHT/2 * math.sin(second_angle)
+    canvas.create_line(WIDTH/2, HEIGHT/2, second_x, second_y, fill="red", width=2)
+
+    canvas.after(1000, update_clock)
+
 
 update_clock()
 root.mainloop()
